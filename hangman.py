6e5d52123f909a4204f3337a7d26ae648f30483b
Shaï Halimi <shai.halimi@gmail.com>
2019-06-10 22:18:29 +0200

    add hangman.py
    Past my code

---
+++ hangman.py
@@ -0,0 +1,91 @@
+import random
+
+print ('We will play hangman game !')
+raw_input('Press Enter to start !\n')
+
+# create clear screen function
+def clearScreen():
+    print ('\n'*100)
+
+# init var
+fault = 0 # max is 10
+used = [] # array of char used
+fund = [] # array of good char fund
+
+# diagram drawing
+#_____
+#|   |
+#|   O
+#|  /|\
+#|   /\
+#|____
+space = '\t'*4
+diagram =[
+ '',
+ space + '____',
+ (space + '|\n')*4 + space + '|____',
+ space + '_____\n' + (space + '|\n')*4 + space + '|____',
+ space + '_____\n' + space + '|   |\n' + (space + '|\n')*3 + space + '|____',
+ space + '_____\n' + space + '|   |\n' + space + '|   O\n' + (space + '|\n')*2 + space + '|____',
+ space + '_____\n' + space + '|   |\n' + space + '|   O\n' + space + '|   |\n' + space + '|\n' + space + '|____',
+ space + '_____\n' + space + '|   |\n' + space + '|   O\n' + space + '|  /|\n' + space + '|\n' + space + '|____',
+ space + '_____\n' + space + '|   |\n' + space + '|   O\n' + space + '|  /|\\\n' + space + '|\n' + space + '|____',
+ space + '_____\n' + space + '|   |\n' + space + '|   O\n' + space + '|  /|\\\n' + space + '|   |\n' + space + '|____',
+ space + '_____\n' + space + '|   |\n' + space + '|   O\n' + space + '|  /|\\\n' + space + '|  /|\n' + space + '|____',
+ space + '_____\n' + space + '|   |\n' + space + '|   O\n' + space + '|  /|\\\n' + space + '|  /|\\\n' + space + '|____',]
+
+# words init
+word = random.choice(['cuivre','vache','canard','ecole','poire','idee','inspiration','pillule'])
+
+# init fund to nothing
+for c in word:
+    fund.append('_')
+
+# start game's loop
+while fault != 11 and word != ''.join(fund):
+
+    clearScreen()
+
+    # print the hangman's draw
+    print diagram[fault]
+
+    # print the word without unfund char
+    print ' '.join(fund)
+
+    # asking for char
+    c = raw_input('Give me a letter !\n')
+
+    # refuse char if he was already used or if it's a string
+    while len(c) != 1 or c in used:
+        if c in used:
+            c = raw_input('You\'ve already used this char !\n')
+        else:
+            c = raw_input('"' + c + '"' + ' is not a char. Give me one !\n')
+
+
+    # add the used char to the used char list
+    used.append(c)
+
+    # check if the char is in the word
+    if c not in word:
+        fault += 1
+    else:
+        # replacing unknown char by known ones
+        i=0
+        for char in word:
+            if char == c:
+                fund[i] = c
+            i += 1
+
+# clear the screen for the score
+clearScreen()
+
+# draw the score
+print diagram[fault]
+print ' '.join(fund)
+
+# check if win
+if fault == 11:
+    raw_input( 'You lost ! Press Enter to quit !')
+else:
+    raw_input('You win ! Press Enter to quit !')
